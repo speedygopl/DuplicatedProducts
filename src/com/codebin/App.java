c6789a742cf58b90bf193a20aa227e5e94a436c1
Damazy Kowalski <damazy5@wp.pl>
2019-12-19 06:35:24 +0100

    Initial commit

---
+++ src/com/codebin/App.java
@@ -0,0 +1,66 @@
+package com.codebin;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+public class App {
+    private JButton buttonRUN;
+    private JPanel PanelMain;
+    private JButton selectFileButton;
+    private JLabel selectFileLabel;
+    private final JFileChooser openFileChooser;
+    protected static String path;
+
+    public App() {
+        openFileChooser = new JFileChooser();
+        openFileChooser.setCurrentDirectory(new File("c:\\users\\damazy\\Downloads"));
+        openFileChooser.setFileFilter(new FileNameExtensionFilter("CSV files", "csv"));
+
+        buttonRUN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    JOptionPane.showMessageDialog(null, startProgram());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        });
+        selectFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int resultValue = openFileChooser.showOpenDialog(null);
+                if (resultValue == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        ImageIO.read(openFileChooser.getSelectedFile());
+                        selectFileLabel.setText("File successfully loaded !!!");
+                    } catch (IOException ioe) {
+                        selectFileLabel.setText("Failed to load the file !!!");
+                    }
+                }else {
+                    selectFileLabel.setText("No file chosen !!!");
+                }
+                    path = openFileChooser.getSelectedFile().getAbsolutePath();
+                }
+            });
+        }
+
+        public String startProgram () throws IOException {
+            Program program = new Program();
+            return program.writeResultToFile();
+        }
+
+        public static void main (String[]args){
+            JFrame frame = new JFrame("Powielone Produkty");
+            frame.setContentPane(new App().PanelMain);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        }
+    }
---
+++ src/com/codebin/Program.java
@@ -0,0 +1,134 @@
+package com.codebin;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Program {
+
+    // method gets text from chosen file
+    public List<String> readAllLinesFromFile() throws IOException {
+        return Files.readAllLines(Paths.get(App.path));
+    }
+
+    //method removes unnecessary text from file
+    public List<String> returnSelectedLines() throws IOException {
+        List<String> allLines = readAllLinesFromFile();
+        return allLines.stream()
+                .filter(x -> !(x.contains("range_labels"))
+                        && !(x.contains("range,2"))
+                        && !(x.contains("transaction_labels"))
+                        && !(x.contains("position_labels,transactionId,Name,Quantity"))
+                        && !(x.contains("subtransaction_labels,")))
+                .map(x -> x.replaceAll("transaction,,", ""))
+                .map(x -> x.replaceAll("subtransaction,", ""))
+                .map(x -> x.replaceAll("position,", ""))
+                .map(x -> x.replaceAll(",[0-9]{8}", ""))
+                .filter(x -> x.length() < 62)
+                .collect(Collectors.toList());
+    }
+
+// method return the list which contains only id number of item and its quantity
+    public List<String> returnQuantityList() throws IOException {
+        List<String> allLines = returnSelectedLines();
+        List<String> quantityList;
+        quantityList = allLines.stream()
+                .filter(x -> x.contains(",,"))
+                .collect(Collectors.toList());
+        FileWriter fileWriter = new FileWriter("d:/quantityList.txt");
+        for (String line : quantityList) {
+            fileWriter.write(line + System.lineSeparator());
+        }
+        fileWriter.close();
+        return quantityList;
+
+    }
+// method returns the list of items' names with corresponding id numbers
+    public List<String> returnProductList() throws IOException {
+        List<String> allLines = returnSelectedLines();
+        List<String> productList;
+        productList = allLines.stream()
+                .filter(x -> !x.contains(",,"))
+                .collect(Collectors.toList());
+        FileWriter fileWriter = new FileWriter("d:/productList.txt");
+        for (String line : productList) {
+            fileWriter.write(line + System.lineSeparator());
+        }
+        fileWriter.close();
+        return productList;
+
+    }
+
+    public List<String> returnProductAndQuantityList() throws IOException {
+        List<String> productList = returnProductList();
+        List<String> quantityList = returnQuantityList();
+        List<String> productAndQuantityList = new ArrayList<>();
+        for (int i = 0; i < quantityList.size(); i++) {
+            String[] quantityArray = quantityList.get(i).split(",,");
+            for (int j = 0; j < productList.size(); j++) {
+                if (productList.get(j).contains(quantityArray[0])) {
+                    productAndQuantityList.add(i, productList.get(j).concat(":::" + quantityArray[1]));
+                    productList.remove(j);
+                }
+            }
+        }
+        FileWriter fileWriter = new FileWriter("d:/productAndQuantityList.txt");
+        for (String line : productAndQuantityList) {
+            fileWriter.write(line + System.lineSeparator());
+        }
+        fileWriter.close();
+        return productAndQuantityList;
+
+    }
+
+    public List<String> getListOfRepeatedNamesAndQuantities() throws IOException {
+        List<String> productAndQuantityList = returnProductAndQuantityList();
+        List<String> resultList = new ArrayList<>();
+        List<String> finalList = new ArrayList<>();
+        Map<String, Integer> namesAndQuantitiesMap = new HashMap<>();
+        productAndQuantityList = productAndQuantityList.stream()
+                .map(x -> x.replaceAll("[0-9]{8},", ""))
+                .collect(Collectors.toList());
+        for (int i = 0; i < productAndQuantityList.size(); i++) {
+            String[] nameAndQuantityArray = productAndQuantityList.get(i).split(":::");
+            if (!namesAndQuantitiesMap.containsKey(nameAndQuantityArray[0])) {
+                namesAndQuantitiesMap.put(nameAndQuantityArray[0], Integer.parseInt(nameAndQuantityArray[1]));
+
+            } else {
+                namesAndQuantitiesMap.put(nameAndQuantityArray[0], namesAndQuantitiesMap.get(nameAndQuantityArray[0]) + Integer.parseInt(nameAndQuantityArray[1]));
+                if (!resultList.contains(nameAndQuantityArray[0]))
+                    resultList.add(nameAndQuantityArray[0]);
+            }
+        }
+
+        for (int i = 0; i < resultList.size(); i++) {
+            finalList.add(resultList.get(i).concat(":::" + namesAndQuantitiesMap.get(resultList.get(i)).toString()));
+        }
+        return finalList;
+
+    }
+
+
+    public String writeResultToFile() throws IOException {
+        List<String> finalList = getListOfRepeatedNamesAndQuantities();
+        String resultOutput = "";
+        for (String line : finalList) {
+            resultOutput += line + System.lineSeparator();
+        }
+        System.out.println(resultOutput);
+        return resultOutput;
+
+    }
+
+
+}
+
+
+
